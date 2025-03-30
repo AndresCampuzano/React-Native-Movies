0a0929e14698d17387fe448ceaec4abd681568b9
Andres Campuzano <campuxano88@gmail.com>
2025-03-30 14:12:28 -0500

    add KeyboardAvoidingView for better usability on mobile

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from 'react';
-import { View, Image, FlatList, ActivityIndicator, Text } from 'react-native';
+import {
+  View,
+  Image,
+  FlatList,
+  ActivityIndicator,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import { images } from '@/constants/images';
 import MovieCard from '@/components/MovieCard';
 import useFetch from '@/services/useFetch';
@@ -26,57 +34,62 @@ const Search = () => {
   }, [searchQuery]);
 
   return (
-    <View className={'flex-1 bg-primary'}>
-      <Image source={images.bg} className={'flex-1 absolute w-full z-0'} resizeMode={'cover'} />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <View className={'flex-1 bg-primary'}>
+        <Image source={images.bg} className={'flex-1 absolute w-full z-0'} resizeMode={'cover'} />
 
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <MovieCard {...item} />}
-        keyExtractor={item => item.id.toString()}
-        numColumns={3}
-        columnWrapperStyle={{
-          gap: 20,
-          paddingRight: 5,
-          marginBottom: 20,
-        }}
-        className={'px-5'}
-        contentContainerStyle={{ paddingBottom: 100 }}
-        ListHeaderComponent={
-          <>
-            <View className={'w-fill flex-row justify-center mt-20 items-center'}>
-              <Image source={icons.logo} className={'w-12 h-10'} />
-            </View>
-            <View className={'my-5'}>
-              <SearchBar
-                placeholder={'Search for a movie...'}
-                value={searchQuery}
-                onChangeText={(text: string) => setSearchQuery(text)}
-              />
-            </View>
-
-            {loading && <ActivityIndicator size={'large'} color={'#0000ff'} className={'my-3'} />}
-
-            {error && <Text className={'text-red-500 px-5 my-3'}>Error: {error.message}</Text>}
-
-            {!loading && !error && searchQuery.trim() && data?.length! > 0 && (
-              <Text className="text-xl text-white font-bold">
-                Search Results for <Text className="text-accent">{searchQuery}</Text>
-              </Text>
-            )}
-          </>
-        }
-        ListEmptyComponent={
-          !loading && !error && searchQuery.trim() ? (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <MovieCard {...item} />}
+          keyExtractor={item => item.id.toString()}
+          numColumns={3}
+          columnWrapperStyle={{
+            gap: 20,
+            paddingRight: 5,
+            marginBottom: 20,
+          }}
+          className={'px-5'}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListHeaderComponent={
             <>
-              <Text className={'text-white text-center mt-5'}>No results found</Text>
-              <Text className={'text-gray-400 text-center mt-2'}>
-                Try searching for something else
-              </Text>
+              <View className={'w-fill flex-row justify-center mt-20 items-center'}>
+                <Image source={icons.logo} className={'w-12 h-10'} />
+              </View>
+              <View className={'my-5'}>
+                <SearchBar
+                  placeholder={'Search for a movie...'}
+                  value={searchQuery}
+                  onChangeText={(text: string) => setSearchQuery(text)}
+                />
+              </View>
+
+              {loading && <ActivityIndicator size={'large'} color={'#0000ff'} className={'my-3'} />}
+
+              {error && <Text className={'text-red-500 px-5 my-3'}>Error: {error.message}</Text>}
+
+              {!loading && !error && searchQuery.trim() && data?.length! > 0 && (
+                <Text className="text-xl text-white font-bold mb-3">
+                  Search Results for <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
             </>
-          ) : null
-        }
-      />
-    </View>
+          }
+          ListEmptyComponent={
+            !loading && !error && searchQuery.trim() ? (
+              <>
+                <Text className={'text-white text-center mt-5'}>No results found</Text>
+                <Text className={'text-gray-400 text-center mt-2'}>
+                  Try searching for something else
+                </Text>
+              </>
+            ) : null
+          }
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
