05d1ab69c2a74f4191f09a99cb819278dff24306
Andres Campuzano <campuxano88@gmail.com>
2025-03-30 22:16:04 -0500

    add StatusBar component for improved UI visibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,25 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import './globals.css';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="movies/[id]"
-        options={{
-          headerShown: true,
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{
+            headerShown: true,
+          }}
+        />
+      </Stack>
+    </>
   );
 }
